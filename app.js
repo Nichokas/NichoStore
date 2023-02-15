a0affebc94ce8cacdaa0beb5da2ff8f80f7a5412
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-02-15 19:44:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+  document.getElementById("download-button").addEventListener("click", function() {
+    // Agregue aquí el código para descargar la aplicación
+    alert("¡Gracias por descargar nuestra aplicación!");
+  });
+  
