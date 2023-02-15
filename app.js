9a9ef9e707722daceeef62d26e3dec1c4865ff70
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-02-15 19:43:24 +0100

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-  document.getElementById("download-button").addEventListener("click", function() {
-    // Agregue aquí el código para descargar la aplicación
-    alert("¡Gracias por descargar nuestra aplicación!");
-  });
-  
