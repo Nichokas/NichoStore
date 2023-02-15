68d2228b3251d87ac6190850b5ba6bce8ce96638
Nichokas <67232385+Nichokas@users.noreply.github.com>
2023-02-15 18:42:24 +0000

    Pending changes exported from your codespace

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// Agrega tu código JavaScript aquí
-import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://c6f8e361b6ec45d98fabc9ee0e3f6623@o4504651365875712.ingest.sentry.io/4504651928764416",
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0
+  document.getElementById("download-button").addEventListener("click", function() {
+    // Agregue aquí el código para descargar la aplicación
+    alert("¡Gracias por descargar nuestra aplicación!");
+  });
+  
